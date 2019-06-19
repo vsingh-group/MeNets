13aded4e0237c8eba18fd5e111835aa4975f096e
Yunyang Xiong <yxiong43@wisc.edu>
2019-06-19 13:07:16 -0700

    Update train_res18_gaze.sh

--- scripts/train_res18_gaze.sh
+++ scripts/train_res18_gaze.sh
@@ -1,4 +1,4 @@
 
 GLOG_logtostderr=1 ../../../build/tools/caffe train \
-  --solver=quicker_solver.prototxt \
-  --gpu=0  2>&1 | tee U_res_log_p02c.txt
+  --solver=solver.prototxt \
+  --gpu=0  2>&1 | tee res_log_p02c.txt
